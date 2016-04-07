a6af62f077da49d8a790dc051308be345057b632
Allen Kramer <kramer.alleng@gmail.com>
2016-04-07 07:26:45 -0400

    added Event model nested under Registered Application, updated RA show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :registered_applications
+  resources :registered_applications do
+    resources :events
+  end
+
   devise_for :users
 
   root 'welcome#index'
